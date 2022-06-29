a181f5ecb1c2c63133305b9ecb974a7a43ccefa3
Tellula <julieloane@gmail.com>
2022-06-29 03:44:29 +0200

    Add logo component WIP

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import brain from "./brain.png"
+import "./logo.css";
+
+const Logo = () => {
+  return (
+    <div className="ma5 mt0 flex justify-center">
+      <Tilt perspective={1000}>
+        <div className="tilt shadow-2 br4">
+          <img className="logo" src={brain} alt="brain logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
