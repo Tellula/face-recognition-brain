80ac650f7328b58f9ecee8e0b3d9212478433339
Tellula <julieloane@gmail.com>
2022-06-29 05:43:10 +0200

    Add placeholder

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-          <input className="f4 pa2 w-70 center" type="text" />
+          <input className="f4 pa2 w-70 center" type="text" placeholder="Enter image url..." />
           <button className="button w30 grow f4 link ph3 pv2 dib white bg-light-purple">
             Detect
           </button>
